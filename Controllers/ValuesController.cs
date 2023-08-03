46ab7ea268d0512f6d8e3775079315c4b559a4fa
zhangyi <zhangyi905@guance.com>
2023-08-03 17:21:23 +0800

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace netcore_2_2_webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var envs = Environment.GetEnvironmentVariables();
+
+            List<string> variables = new List<string>(envs.Count);
+
+            foreach (var key in envs.Keys)
+            {
+                variables.Add(String.Format("{0} : {1}", key, envs[key]));
+            }
+
+            return variables;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- run.sh
+++ run.sh
@@ -1,14 +1,7 @@
-# netcore_2_2_webapi
-.NET Core 2.2 profiling demo
-
-
-## Build and run on Linux amd64
-
-```shell
-cd netcore_2_2_webapi 
-dotnet publish -c Release
+#!/usr/bin/env bash
 
 DDTRACE_HOME="$(pwd)/bin/Release/netcoreapp2.2/publish/datadog";
+
 DD_DOTNET_TRACER_HOME="$DDTRACE_HOME" \
 CORECLR_ENABLE_PROFILING=1 \
 CORECLR_PROFILER="{846F5F1C-F9AE-4B07-969E-05C26BC060D8}" \
@@ -25,6 +18,3 @@ DD_PROFILING_GC_ENABLED=1 \
 DD_SERVICE=dotnet-profiling-demo DD_ENV=testing DD_VERSION=1.2.3 \
 DD_AGENT_HOST=127.0.0.1 DD_TRACE_AGENT_PORT=9529 \
 dotnet bin/Release/netcoreapp2.2/publish/netcore_2_2.dll
-
-
-```
